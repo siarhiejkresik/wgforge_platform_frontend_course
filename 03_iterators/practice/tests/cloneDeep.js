80743f81a84c6d0f02550aa1862626fee642d2a5
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-04-16 14:32:32 +0300

    Add tests for cloneDeep

--- 03_iterators/practice/tests/cloneDeep.js
+++ 03_iterators/practice/tests/cloneDeep.js
@@ -1,3 +1,4 @@
+/* eslint-disable filenames/match-regex */
 import test from 'tape-catch';
 import cloneDeep from '../exercises/cloneDeep';
 
@@ -34,6 +35,47 @@ test('cloneDeep', t => {
       false
     );
     tt.equal(clonedInstance.forgeMaintaince[0] === sourceObject.forgeMaintaince[0], false);
+
+    tt.deepEqual(sourceObject, clonedInstance, 'source and cloned should be equivalent');
+
+    tt.end();
+  });
+
+  t.test('deep clone a map', tt => {
+    const objKey = { c: 10 };
+
+    const source = new Map();
+    source.set('a', { x: 1 });
+    source.set('b', 123);
+    source.set(objKey, 'text');
+
+    const cloned = cloneDeep(source);
+
+    tt.deepEqual(source, cloned, 'should clone maps');
+    tt.false(source === cloned, 'shouldn’t be the same object');
+    tt.false(cloned.has(objKey), 'should clone map keys if objects');
+    tt.false(source.get('a') === cloned.get('a'), 'should clone map values if objects');
+
+    tt.end();
+  });
+
+  t.test('deep clone a set', tt => {
+    const obj = { x: 1 };
+    const source = new Set([1, 'qwe', obj]);
+    const cloned = cloneDeep(source);
+
+    tt.deepEqual(source, cloned, 'should clone sets');
+    tt.false(cloned.has(obj), 'should clone values if objects');
+
+    tt.end();
+  });
+
+  t.test('deep clone a date object', tt => {
+    const source = new Date();
+    const cloned = cloneDeep(source);
+
+    tt.deepEqual(source, cloned, 'should clone date objects');
+    tt.false(source === cloned, 'shouldn’t be the same object');
 
     tt.end();
   });
